b338a2a6cfa6d64218dbfd896a5b9522f91dd35e
Baozisoftware <->
2017-04-11 21:04:09 +0800

    update

---
+++ jobject.go
@@ -0,0 +1,52 @@
+package jobject
+
+import "encoding/json"
+
+type jObject map[string]interface{}
+
+func PruseJSON(data string) *jObject {
+	var o interface{}
+	if json.Unmarshal([]byte(data), &o) == nil {
+		if m, ok := o.(map[string]interface{}); ok {
+			j := jObject(m)
+			return &j
+		}
+	}
+	return nil
+}
+
+func (v *jObject) jToken(key string) *jObject {
+	t, ok := (*v)[key]
+	if ok {
+		if m, ok := t.(map[string]interface{}); ok {
+			j := jObject(m)
+			return &j
+		}
+	}
+	return nil
+}
+
+func (v *jObject) jArray(key string) []interface{} {
+	t, ok := (*v)[key]
+	if ok {
+		if m, ok := t.([]interface{}); ok {
+			return m
+		}
+	}
+	return nil
+}
+
+func (v *jObject) jTokens(key string) []*jObject {
+	t := v.jArray(key)
+	if t != nil {
+		r := make([]*jObject, 0)
+		for _, v := range t {
+			if m, ok := v.(map[string]interface{}); ok {
+				j := jObject(m)
+				r = append(r, &j)
+			}
+		}
+		return r
+	}
+	return nil
+}
