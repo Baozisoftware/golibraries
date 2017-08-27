1d2da11c34bb6327f67dea406ee67e70ebbe2a1e
Baozisoftware <->
2017-08-27 12:16:23 +0800

    update

--- scall/scall_shared.go
+++ scall/scall_shared.go
@@ -3,6 +3,7 @@ package scall
 import (
 	"os/exec"
 	"os"
+	"path"
 )
 
 func CreateProcess(prog string, args ...string) (p *os.Process, err error) {
@@ -10,6 +11,18 @@ func CreateProcess(prog string, args ...string) (p *os.Process, err error) {
 	err = cmd.Start()
 	if err == nil {
 		p = cmd.Process
+	}
+	return
+}
+
+func CreateDir(dirpath string) error {
+	return os.MkdirAll(dirpath, os.ModePerm)
+}
+
+func CreateFile(filepath string) (file *os.File, err error) {
+	err = CreateDir(path.Dir(filepath))
+	if err == nil {
+		file, err = os.Create(filepath)
 	}
 	return
 }
