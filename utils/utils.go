ac4b501db1e0f51651489dc6f32f386187087ee0
Baozisoftware <hlj493595708@hotmail.com>
2018-06-20 13:17:14 +0800

    update

--- utils/utils.go
+++ utils/utils.go
@@ -10,7 +10,7 @@ import (
 func GetRandomString(l int) string {
 	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	bytes := []byte(str)
-	result := []byte{}
+	var result []byte
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := 0; i < l; i++ {
 		result = append(result, bytes[r.Intn(len(bytes))])
